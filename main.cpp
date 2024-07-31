d950559edcf2ded69a5e97ccedd8ea95a03af338
noinline <139589029+noinline@users.noreply.github.com>
2024-07-31 22:36:58 +0700

    perhaps so called almost finished program

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <exception>
 #include <iostream>
 #include <sys/ioctl.h>
 
@@ -54,7 +53,7 @@ get_shell()
 std::string
 get_terminal_emulator()
 {
-  std::string output = getenv("TERM");
+  const std::string output = getenv("TERM");
   return output.c_str();
 }
 
@@ -69,10 +68,82 @@ get_file_content(std::istream &s, std::string output)
   return output.c_str();
 }
 
+#include <regex>
+
+std::string
+get_system_name()
+{
+  std::string name_cmd = "lsb_release -a";
+
+  FILE *name_pipe = popen(name_cmd.c_str(), "r");
+  if (!name_pipe)
+    return "";
+
+  std::string name_output;
+  while (!feof(name_pipe)) {
+    char buffer[128];
+    if (fgets(buffer, sizeof(buffer), name_pipe) != NULL)
+      name_output += buffer;
+  }
+  pclose(name_pipe);
+
+  std::regex  system_name_pattern("Distributor ID:\\s*(\\w+)");
+  std::smatch matches;
+  if (std::regex_search(name_output, matches, system_name_pattern))
+    return matches.str(1);
+
+  return "unknown";
+}
+
+std::string
+get_window_manager()
+{
+  std::string check_cmd = "xprop -root _NET_SUPPORTING_WM_CHECK";
+  std::string name_cmd = "xprop -id $(xprop -root _NET_SUPPORTING_WM_CHECK | "
+                         "awk '{print $NF}') _NET_WM_NAME";
+
+  FILE *check_pipe = popen(check_cmd.c_str(), "r");
+  if (!check_pipe)
+    return "";
+
+  std::string check_output;
+  while (!feof(check_pipe)) {
+    char buffer[128];
+    if (fgets(buffer, sizeof(buffer), check_pipe) != NULL)
+      check_output += buffer;
+  }
+  pclose(check_pipe);
+
+  std::regex window_id_pattern(
+      "_NET_SUPPORTING_WM_CHECK\\(WINDOW\\): window id \\# 0x([a-fA-F0-9]+)");
+  std::smatch matches;
+  if (std::regex_search(check_output, matches, window_id_pattern)) {
+    std::string window_id_pattern = matches.str(1);
+
+    FILE *name_pipe = popen(name_cmd.c_str(), "r");
+    if (!name_pipe)
+      return "";
+
+    std::string name_output;
+    while (!feof(name_pipe)) {
+      char buffer[128];
+      if (fgets(buffer, sizeof(buffer), name_pipe) != NULL)
+        name_output += buffer;
+    }
+    pclose(name_pipe);
+
+    std::regex name_pattern("_NET_WM_NAME\\(UTF8_STRING\\) = \"([^\"]+)\"");
+    if (std::regex_search(name_output, matches, name_pattern))
+      return matches.str(1);
+  }
+
+  return "unknown";
+}
+
 #include <sys/stat.h>
 
 void
-create_directory(std::string dir)
+create_directory(const std::string &dir)
 {
   struct stat st = {0};
   if (stat(dir.c_str(), &st) != -1)
@@ -84,7 +155,7 @@ create_directory(std::string dir)
 }
 
 void
-print_image(int width, int height, std::string image_path)
+print_image(int width, int height, const std::string &image_path)
 {
   constexpr int xoffset = 2, yoffset = 0;
   get_terminal_size(width, height);
@@ -96,8 +167,9 @@ print_image(int width, int height, std::string image_path)
   system("clear");
   system(command.c_str());
 }
+
 void
-printf_tabbed(std::string str)
+printf_tabbed(const std::string &str)
 {
   printf(" 			%s\n", str.c_str());
 }
@@ -109,6 +181,24 @@ print_welcome()
   output += " ";
   output += get_user_name() + "@" + get_node_name();
   output += "!";
+  return output.c_str();
+}
+
+std::string
+print_system_name()
+{
+  std::string output{" >"};
+  output += " ";
+  output += get_system_name();
+  return output.c_str();
+}
+
+std::string
+print_window_manager()
+{
+  std::string output{" >"};
+  output += " ";
+  output += get_window_manager();
   return output.c_str();
 }
 
@@ -170,10 +260,12 @@ main(int argc, char *argv[])
     const std::string image_path = dir + "chibi.png";
     print_image(width, height, dir);
     printf_tabbed(print_welcome());
+    printf_tabbed(print_system_name());
+    printf_tabbed(print_window_manager());
     printf_tabbed(print_terminal());
     printf_tabbed(print_kernel());
     printf_tabbed(print_shell());
-    printf("\033[%iB\n", 5);
+    printf("\033[%iB\n", 1);
   } break;
   case 'h':
     printf("Usage:\n	"
